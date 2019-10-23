644f363b9aeb530787924e7dcc224a91fb913b1c
Jordan Lamb <jdawg2329@gmail.com>
2019-10-23 14:08:37 -0600

    Gameboard started

---
+++ Chris/Chris/Custom Views/ScoreBoardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScoreBoardTableViewCell.swift
+//  Chris
+//
+//  Created by Jordan Lamb on 10/23/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class ScoreBoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
