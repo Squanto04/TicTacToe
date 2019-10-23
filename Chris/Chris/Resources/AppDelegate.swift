cd9d8cced699b7f76c5a430fb8cfecd8131ccd2b
Chris Alegre <calegre14@gmail.com>
2019-10-23 12:24:43 -0600

    Adds Model and Model Controllers for Player and Score. Created Crud Functions and save/load functions Loaded persistence called in AppDelegate

--- Chris/Chris/Resources/AppDelegate.swift
+++ Chris/Chris/Resources/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        PlayerController.shared.loadFromPersistantStorage()
         return true
     }
 
