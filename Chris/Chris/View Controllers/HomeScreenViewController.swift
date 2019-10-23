12566dbcd1657fa2028c8a8ff2a03598c91307a7
Jordan Lamb <jdawg2329@gmail.com>
2019-10-23 12:35:34 -0600

    Adds Storyboards and View Controller files for: HomeScreen Scoreboard Gameboard

---
+++ Chris/Chris/View Controllers/HomeScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeScreenViewController.swift
+//  Chris
+//
+//  Created by Jordan Lamb on 10/23/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
