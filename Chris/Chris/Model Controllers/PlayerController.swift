ded378ac3dc48524409f928da260c7fef658dbc0
Chris Alegre <calegre14@gmail.com>
2019-10-23 18:38:12 -0600

    Update for controllers

--- Chris/Chris/Model Controllers/PlayerController.swift
+++ Chris/Chris/Model Controllers/PlayerController.swift
@@ -18,37 +18,5 @@ class PlayerController {
     func createPlayer(name: String, icon: Bool) {
         let newPlayer = Player(name: name, icon: icon)
         players.append(newPlayer)
-        saveToPersistantStore()
-    }
-    
-    func createFileForPersistence() -> URL {
-        //Grab users document directory
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        //Create new fileURL on users phone
-        let fileURL = urls[0].appendingPathComponent("Chris.json")
-        return fileURL
-    }
-
-    func saveToPersistantStore() {
-        let jsonEncoder = JSONEncoder()
-        
-        do {
-            let jsonChris = try jsonEncoder.encode(players)
-            try jsonChris.write(to: createFileForPersistence())
-        } catch let encodingError {
-            print("Error: Could not save data, \(encodingError.localizedDescription)")
-        }
-    }
-    
-    func loadFromPersistantStorage() {
-        let jsonDecoder = JSONDecoder()
-        
-        do {
-            let jsonData = try Data(contentsOf: createFileForPersistence())
-            let decodeChrisData = try jsonDecoder.decode([Player].self, from: jsonData)
-            players = decodeChrisData
-        } catch let decodingError {
-            print("Error: Could not retrieve data from storage, \(decodingError.localizedDescription)")
-        }
     }
 }
--- Chris/Chris/Model Controllers/ScoreController.swift
+++ Chris/Chris/Model Controllers/ScoreController.swift
@@ -17,6 +17,38 @@ class ScoreController {
     func setScore(score: Int, player: Player) {
         let newScore = Score(score: score, player: player)
         scores.append(newScore)
-        PlayerController.shared.saveToPersistantStore()
+        saveToPersistantStore()
     }
+    
+    func createFileForPersistence() -> URL {
+        //Grab users document directory
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        //Create new fileURL on users phone
+        let fileURL = urls[0].appendingPathComponent("Chris.json")
+        return fileURL
+    }
+
+    func saveToPersistantStore() {
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonChris = try jsonEncoder.encode(scores)
+            try jsonChris.write(to: createFileForPersistence())
+        } catch let encodingError {
+            print("Error: Could not save data, \(encodingError.localizedDescription)")
+        }
+    }
+    
+    func loadFromPersistantStorage() {
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            let jsonData = try Data(contentsOf: createFileForPersistence())
+            let decodeChrisData = try jsonDecoder.decode([Score].self, from: jsonData)
+            scores = decodeChrisData
+        } catch let decodingError {
+            print("Error: Could not retrieve data from storage, \(decodingError.localizedDescription)")
+        }
+    }
+    
 }
